5f46ad409ed6c241c668b05efd8f92c21f8c4090
JBOWLN2 <39239815+JBOWLN2@users.noreply.github.com>
2019-01-02 19:54:04 -0500

    Update core.py

--- core.py
+++ core.py
@@ -1,23 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # importing regex and random libraries
+
 import re
 import random
 
 # potential negative responses
-negative_responses = ("no", "nope", "nah", "naw", "not a chance", "sorry")
-# keywords for exiting the conversation 
-exit_commands = ("quit", "pause", "exit", "goodbye", "bye", "later")
-# random starter questions
-random_questions = (
-    "Why are you here? ", 
-    "Are there many humans like you? ", 
-    "What do you consume for sustenance? ", 
-    "Is there intelligent life on this planet? ", 
-    "Does Earth have a leader? ",
-    "What planets have you visited? ",
-    "What technology do you have on this planet?"
+negative_responses = (
+    'no',
+    'nope',
+    'nah',
+    'naw',
+    'not a chance',
+    'sorry',
     )
 
-name = ""
+# keywords for exiting the conversation
+exit_commands = (
+    'quit',
+    'pause',
+    'exit',
+    'goodbye',
+    'bye',
+    'later',
+    )
+
+# random starter questions
+random_questions = (
+    'Why are you here? ',
+    'Are there many humans like you? ',
+    'What do you consume for sustenance? ',
+    'Is there intelligent life on this planet? ',
+    'Does Earth have a leader? ',
+    'What planets have you visited? ',
+    'What technology do you have on this planet?',
+    )
+
+name = ''
 
 alienbabble = (
     # Your planet...
@@ -69,72 +88,78 @@ alienbabble = (
     }
 )
 
+
 # Define greet() below:
 def greet():
-  name = input("Hello, please tell me your name? ")
+    name = input('Hello, please tell me your name? ')
 
-  will_help = input("Hi {}, I'm Etcetera. I'm not from this planet. Will you help me learn about your planet? ".format(name))
-  
-  if will_help in negative_responses:
-    print ("Ok, have a nice Earth day!")
-    return
-  return True
+    will_help = \
+        input("Hi {}, I'm Etcetera. I'm not from this planet. Will you help me learn about your planet? ".format(name))
+
+    if will_help in negative_responses:
+        print('Ok, have a nice Earth day!')
+        return
+    return True
+
 
 # Define make_exit() here:
 def make_exit(reply):
-  for i in exit_commands:
-    if reply == i:
-      print("I have to go now. Bye!")
-      return True
+    for i in exit_commands:
+        if reply == i:
+            print('I have to go now. Bye!')
+            return True
 
 
 # Define alienbot() next:
 def alienbot():
-  if greet() is True:
-    reply = input(random.choice(random_questions)).lower()
-    while make_exit(reply) is not True:
-      reply = converse(reply)
+    if greet() is True:
+        while True:
+            reply = input(random.choice(random_questions)).lower()
+            if make_exit(reply) is True:
+                break
+            answer = converse(reply)
+            print(answer)
 
 
 # Define converse() below:
-
 def converse(reply):
-  for pair in alienbabble:
-    for regex_pattern, alien_answers in pair.items():
-      found_match = re.match(regex_pattern, reply)
-      if found_match:
-        alien_answer = random.choice(alien_answers)
-        reply = input(formatted_alien_answer).lower()
-      formatted_alien_answer = alien_answer.format(*[reflect(matching_group) 						for matching_group in found_match.groups()])
-      return reply
-        
-    
+    for pair in alienbabble:
+        for (regex_pattern, alien_answers) in pair.items():
+            found_match = re.match(regex_pattern, reply)
+            if found_match:
+                alien_answer = random.choice(alien_answers)
+                formatted_alien_answer = \
+                    alien_answer.format(*[reflect(matching_group)
+                        for matching_group in found_match.groups()])
+                return formatted_alien_answer
 
-# dictionary used to switch pronouns 
+
+# dictionary used to switch pronouns
 # and verbs in responses
 reflections = {
-    "i'm": "you are",
+    "i'm": 'you are',
     "you're": "i'm",
-    "was": "were",
-    "i": "you",
-    "are": "am",
-    "am": "are",
-    "i'd": "you would",
-    "i've": "you have",
-    "i'll": "you will",
-    "my": "your",
-    "you've": "I have",
-    "you'll": "I will",
-    "your": "my",
-    "yours": "mine",
-    "you": "I",
-    "me": "you"
-}
+    'was': 'were',
+    'i': 'you',
+    'are': 'am',
+    'am': 'are',
+    "i'd": 'you would',
+    "i've": 'you have',
+    "i'll": 'you will',
+    'my': 'your',
+    "you've": 'I have',
+    "you'll": 'I will',
+    'your': 'my',
+    'yours': 'mine',
+    'you': 'I',
+    'me': 'you',
+    }
+
 def reflect(response):
-  words = response.split()
-  for index, word in enumerate(words):
-    if word in reflections:
-      words[index] = reflections[word]
-  return ' '.join(words)
+    words = response.split()
+    for (index, word) in enumerate(words):
+        if word in reflections:
+            words[index] = reflections[word]
+    return ' '.join(words)
 
 alienbot()
